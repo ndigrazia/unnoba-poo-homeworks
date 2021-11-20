3fcadd0f3a53dd27183c763ea08b504c9c225e88
Nelson Di Grazia <nelson.digrazia@telefonica.com>
2021-11-20 13:49:19 -0300

    Login project

---
+++ 2021/Login/Login/src/main/java/ar/edu/unnoba/pergamino/poo/Login/User.java
@@ -0,0 +1,72 @@
+package ar.edu.unnoba.pergamino.poo.Login;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String username;
+	private String email;
+	private String password;
+	private String role;
+	private String firstName;
+	private String lastName;
+		
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", email=" + email + ", password=" + password + ", role="
+				+ role + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+		
+}
---
+++ 2021/Login/Login/src/test/java/ar/edu/unnoba/pergamino/poo/Login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.pergamino.poo.Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
